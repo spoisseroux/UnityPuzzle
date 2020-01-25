8a243461d811a8cac5ac70045404cb40642e7633
Spencer Poisseroux <54379749+spoisseroux@users.noreply.github.com>
2020-01-25 02:58:45 -0500

    first commit

---
+++ Assets/PlayerMovement/EpicMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+ 
+public class EpicMovement : MonoBehaviour
+{
+    public CharacterController _controller;
+    public float _speed = 10;
+    public float _rotationSpeed = 180;
+ 
+    private Vector3 rotation;
+
+    private Vector3 move = Vector3.zero;
+    public float gravity = 20f;
+    public float jumpSpeed = 8.0f;
+ 
+    public void Update()
+    {
+        if (_controller.isGrounded) {
+            this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
+ 
+            Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+            move = this.transform.TransformDirection(move);
+            _controller.Move(move * _speed);
+            this.transform.Rotate(this.rotation);
+            //if (Input.GetButton("Jump"))
+            //     move.y = jumpSpeed;
+        }
+        move.y -= gravity * Time.deltaTime;
+        _controller.Move(move * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerMovement/IdleToRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToRun : MonoBehaviour
+{
+    public Animator animator;
+    //public float InputX;
+    public float InputY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get animator
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputY = Input.GetAxis("Vertical");
+        animator.SetFloat("InputY", InputY);
+        //InputX = Input.GetAxis("Horizontal");
+        //animator.SetFloat("InputX", InputX);
+    }
+}
